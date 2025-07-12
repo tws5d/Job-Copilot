674d5578db301ef3251c5bd178afd07265b5a737
tws5d <tylersnead85@gmail.com>
2025-07-11 18:58:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ st.markdown("<h1 style='text-align: center;'>Job Application Copilot</h1>", unsa
 col1, col2 = st.columns([3, 4])
 
 with col1:
-    with st.expander("📘 How to Use This Tool", expanded=False):
-        st.markdown("""
-        <div style='text-align: left;'>
-            <p><strong>Step 1:</strong> Set your job preferences</p>
-            <p><strong>Step 2:</strong> Paste a job description</p>
-            <p><strong>Step 3:</strong> Upload Resume</p>
-            <p><strong>Step 4:</strong> Click "GO" to assess Fit</p>
-            <p><strong>Step 5:</strong> (Optional) Generate a tailored cover letter or resume</p>
-        </div>
-        """, unsafe_allow_html=True)
+  with st.expander("📘 How to Use This Tool", expanded=False):
+      st.markdown("""
+      <div style='text-align: left;'>
+          <p><strong>Step 1:</strong> Set your job preferences</p>
+          <p><strong>Step 2:</strong> Paste a job description</p>
+          <p><strong>Step 3:</strong> Upload Resume</p>
+          <p><strong>Step 4:</strong> Click "GO" to assess Fit</p>
+          <p><strong>Step 5:</strong> (Optional) Generate a tailored cover letter or resume</p>
+      </div>
+      """, unsafe_allow_html=True)
 
 with col2:
     st.markdown("""
