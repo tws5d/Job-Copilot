39d0e410eaaef2b140e34cc14ff793e208a4d673
tws5d <tylersnead85@gmail.com>
2025-07-11 18:09:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ with col1:
             <p><strong>Step 1:</strong> Set your job preferences</p>
             <p><strong>Step 2:</strong> Paste a job description</p>
             <p><strong>Step 3:</strong> Upload Resume</p>
-            <p><strong>Step 4:</strong> Click GO to assess Fit</p>
+            <p><strong>Step 4:</strong> Click "GO" to assess Fit</p>
             <p><strong>Step 5:</strong> (Optional) Generate a tailored cover letter or resume</p>
         </div>
         """, unsafe_allow_html=True)
