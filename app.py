19625adcc63e8514cfec5b101c947e5ed00bceda
tws5d <tylersnead85@gmail.com>
2025-07-11 19:02:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ with col1:
 
 with col2:
     st.markdown("""
-        <h4 style='text-align: center; line-height: 1.1; margin-top: -0.5rem; margin-bottom: -0.2rem; font-size: 1.1rem;'>
+        <h3 style='text-align: center; line-height: 1.1; margin-top: -0.5rem; margin-bottom: -0.2rem; font-size: 0.75rem;'>
             💰 Desired Minimum Salary
-        </h4>
+        </h3>
     """, unsafe_allow_html=True)
