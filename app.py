2abcd0901c26be9d1de66d66d9e3f898868f696f
tws5d <tylersnead85@gmail.com>
2025-07-11 19:29:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,24 @@ with col1:
         """, unsafe_allow_html=True)
 
 with col2:
+    # keep the 2:1 split
     sub1, sub2 = st.columns([2, 1])
+
+    # tighten up the right- and bottom-margins here
     sub1.markdown(
-        "<h3 style='text-align: left; line-height: 1.1; margin:0; font-size: 1.25rem;'>💰 Desired Salary</h3>",
+        """
+        <h3 style="
+            text-align: left;
+            margin: 0 0.5rem -0.5rem 0;
+            font-size: 1.25rem;
+        ">
+            💰 Desired Salary
+        </h3>
+        """,
         unsafe_allow_html=True
     )
-    salary = sub2.text_input("", placeholder="E.g., $95,000")
 
-    # now any widgets here will span the full width of col2
-    # e.g.:
-    # st.write("…other stuff below…")
+    # empty label, placeholder text
+    salary = sub2.text_input("", placeholder="E.g., $95,000")
+    
+    # any further widgets here will span the full width of col2
