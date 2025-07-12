8c8df6eb00615403d5ced28a7419949ccfeb0f31
tws5d <tylersnead85@gmail.com>
2025-07-11 18:08:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,12 +8,11 @@ with col1:
     with st.expander("📘 How to Use This Tool", expanded=False):
         st.markdown("""
         <div style='text-align: left;'>
-            <p><strong>Step 1:</strong> Set your job preferences below</p>
+            <p><strong>Step 1:</strong> Set your job preferences</p>
             <p><strong>Step 2:</strong> Paste a job description</p>
             <p><strong>Step 3:</strong> Upload Resume</p>
-            <p><strong>Step 4:</strong> Click GO to see your Fit Score</p>
-            <p><strong>Step 5:</strong> Review feedback</p>
-            <p><strong>Step 6:</strong> (Optional) Generate a tailored cover letter or resume</p>
+            <p><strong>Step 4:</strong> Click GO to assess Fit</p>
+            <p><strong>Step 5:</strong> (Optional) Generate a tailored cover letter or resume</p>
         </div>
         """, unsafe_allow_html=True)
 
